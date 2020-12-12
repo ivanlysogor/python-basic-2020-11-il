ad282ad840d8c871cc966c03d8ab6c1efe5a2486
Ivan Lysogor <ilysogor@yandex-team.ru>
2020-12-12 17:27:35 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# python-basic-2020-11-il
+### Otus python basic course homework (Ivan Lysogor)
+#### Homework 1
+Code output
+```
+Executing function list power with following argc ([1, 2, 3, 4, 5], 2)
+[1, 4, 9, 16, 25]
+
+Execuring function select numbers with following argc ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)
+Function select_numbers execution time is 0.0000069141
+[1, 2, 3, 5, 7]
+
+Execuring function select numbers with following argc ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)
+Function select_numbers execution time is 0.0000028610
+[2, 4, 6, 8, 10]
+
+Execuring function select numbers with following argc ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)
+Function select_numbers execution time is 0.0000030994
+[1, 3, 5, 7, 9]
+
+Executing function fib to get Fibonacchi numbers with following argc (50,)
+--->fib_loop(0)
+-------->fib_loop(1)
+------------->fib_loop(2)
+------------------>fib_loop(3)
+----------------------->fib_loop(4)
+---------------------------->fib_loop(5)
+--------------------------------->fib_loop(6)
+<---------------------------------fib_loop(6)
+<----------------------------fib_loop(5)
+<-----------------------fib_loop(4)
+<------------------fib_loop(3)
+<-------------fib_loop(2)
+<--------fib_loop(1)
+<---fib_loop(0)
+[1, 2, 3, 5, 8, 13, 21, 34]
+```
---
+++ hw1/main.py
@@ -0,0 +1,140 @@
+import time
+import operator
+from functools import wraps
+import pprint
+
+EVEN = 1
+ODD = 2
+PRIME = 3
+
+def list_powers(numbers, power = 2):
+    """
+    Powers a list of numbers
+    :param numbers: a list of number to power
+    :param power: expected power (default is 2)
+    :return: the new list of numbers
+    """
+    return list(map(operator.pow,numbers,[power] * len(numbers)))
+
+def is_prime(x):
+    """
+    Checks if number is a prime
+    :param x: A number to check
+    :return: True|False
+    """
+    if x<4:
+        return True
+    if x % 2 == 0:
+        return False
+    for i in range(3,x//2,2):
+        if x % i == 0:
+            return False
+    return True
+
+def is_even(x):
+    """
+    Checks if number is even
+    :param x: A number to check
+    :return: True|False
+    """
+    if x % 2 == 0:
+        return True
+    return False
+
+def is_odd(x):
+    """
+    Checks if number is odd
+    :param x: A number to check
+    :return: True|False
+    """
+    if x % 2 != 0:
+        return True
+    return False
+
+def time_estimation(func):
+    """
+    Decorator to add function execution time
+    :param func: A func to decorate
+    :return: A func result
+    """
+    @wraps(func)
+    def estimation(*argc, **kargc):
+        start_time=time.time()
+        result = func(*argc, **kargc)
+        delta_time=time.time()-start_time
+        print(f'Function {func.__name__} execution time is {delta_time:.10f}')
+        return result
+    return estimation
+
+@time_estimation
+def select_numbers(numbers, number_type = EVEN):
+    """
+    Select en ODD, EVEN or SIMPLE numbers from a list
+    :param numbers:  a list of numbers
+    :param number_type: type of numbers to select (default = EVEN)
+    :return: a filtered list
+    """
+    return list(filter({
+                           number_type == EVEN: is_even,
+                           number_type == ODD: is_odd,
+                           number_type == PRIME: is_prime,
+                        }[True],numbers))
+
+
+
+def trace(func):
+    """
+    Decorator to show fibo nested level
+    :param func: A func to decorate
+    :return: A func result
+    """
+    @wraps(func)
+    def wrapper(*argc, **kargc):
+        print(f"{'-----' * (len(argc[0]) - 2)}--->{func.__name__}({len(argc[0]) - 2})")
+        res=func(*argc, **kargc)
+        print(f"<---{'-----' * (len(argc[0]) - 2)}{func.__name__}({len(argc[0]) - 2})")
+        return res
+    return wrapper
+
+def fib(upto):
+    """
+    Calculated fibo list of numbers less then upto
+    :param upto: a max number to calcute (calculate all fibo numbers less than upto)
+    :return: a fibo list
+    """
+    @trace
+    def fib_loop(fib_list,upto):
+        if fib_list[-1] + fib_list[-2] < upto:
+            return fib_loop(fib_list + [fib_list[-1] + fib_list[-2]] ,upto)
+        else:
+            return fib_list
+    if upto < 1:
+        return []
+    if upto < 2:
+        return [1]
+    return fib_loop([1,2], upto)
+
+def comment(func, comment):
+    @wraps(func)
+    def wrapper(*argc, **kargc):
+        print(f'{comment} with following argc {argc}')
+        res=func(*argc, **kargc)
+        pprint.pprint(res)
+        print()
+        return res
+    return wrapper
+
+if __name__ == "__main__":
+
+    c_list_powers=comment(list_powers,"Executing function list power")
+    c_list_powers([1,2,3,4,5],2)
+
+    numbers=[1,2,3,4,5,6,7,8,9,10]
+
+    c_select_numbers=comment(select_numbers,"Execuring function select numbers")
+    c_select_numbers(numbers, PRIME)
+    c_select_numbers(numbers, EVEN)
+    c_select_numbers(numbers, ODD)
+
+    c_fib=comment(fib,'Executing function fib to get Fibonacchi numbers')
+    c_fib(50)
